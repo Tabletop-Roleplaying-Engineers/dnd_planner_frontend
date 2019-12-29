278e56751ef7c25a66eb45657362abe0a6c43fd9
rosty <blackswordgc@gmail.com>
2019-12-29 18:13:09 +0200

    Pre-release 1

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80f7d9133986a8a518de87d966c06404.js"
+  "/precache-manifest.ebc9268ca29a43dea8c018d230fb2569.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
