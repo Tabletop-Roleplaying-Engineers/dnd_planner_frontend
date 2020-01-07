159f1a8e4f5c6f55c8fb6e06cc294de12f38e6bd
rosty <blackswordgc@gmail.com>
2020-01-07 20:50:35 +0200

    Release: New Calendar!

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12b373d995ed7e4f7245c7ad591a2fd1.js"
+  "/precache-manifest.d678193e67915ddd6911fa655ff79a8d.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
