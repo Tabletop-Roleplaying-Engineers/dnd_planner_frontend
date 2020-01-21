fd5dfff70fd6445f48b7b7869d3de83915aa0b67
Dmytro Borchuk <frunk.lern@gmail.com>
2020-01-22 00:01:55 +0200

    Fixed calendar cell of previous and next perion (#39)

--- src/components/DateRange/DateRange.js
+++ src/components/DateRange/DateRange.js
@@ -2,27 +2,11 @@ import React from 'react'
 import format from 'date-fns/format'
 import startOfWeek from 'date-fns/startOfWeek'
 import endOfWeek from 'date-fns/endOfWeek'
-import styled from 'styled-components'
 import { ViewType } from 'react-responsive-calendar'
-import { Box, Flex } from 'noui/Position'
-
-const Wrapper = styled(Flex)`
-  cursor: pointer;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  overflow: auto;
-`
-const Title = styled(Box)`
-  padding: 5px 10px;
-`
+import { Flex } from 'noui/Position'
 
 const getMonthRange = date => format(date, 'MMMM y')
 const getWeekRange = (date) => {
-  // 13th March - 20
   const start = startOfWeek(date)
   const end = endOfWeek(date)
   return `${format(start, 'do MMMM y')} - ${format(end, 'do MMMM y')}`
