c5f0cd3ca9f8c05efd79f49abda97b7367acab50
Dmytro Borchuk <frunk.lern@gmail.com>
2020-01-26 14:26:21 +0200

    Make create character for m responsive

--- src/config.js
+++ src/config.js
@@ -2,4 +2,9 @@ import * as R from 'ramda'
 
 export const playersInGame = [3, 4, 5, 6]
 
-export const modalWidth = () => R.min(document.body.clientWidth * 0.8, 640)
+export const modalWidth = () => {
+  if (document.body.clientWidth > 375) {
+    return R.min(document.body.clientWidth * 0.8, 640)
+  }
+  return '100%'
+}
