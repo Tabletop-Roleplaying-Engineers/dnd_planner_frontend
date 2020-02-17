fcadcdee9113da1e354f3efe012a6a78bc9beff6
Dmytro Borchuk <frunk.lern@gmail.com>
2020-02-17 14:45:16 +0200

    Fixed game message markup (#51)

--- src/components/Game/ParticipantsList.js
+++ src/components/Game/ParticipantsList.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Col, Card } from 'antd'
+import { Row, Col, Card } from 'antd'
 import Character from 'components/Character'
 
 export const ParticipantsList = ({ characters }) => {
   return characters.map(character => (
+    <Row>
       <Col md={12} key={character.id}>
         <Card size="small" bordered={false}>
           <Character {...character} />
         </Card>
       </Col>
+    </Row>
   ))
 }
 
