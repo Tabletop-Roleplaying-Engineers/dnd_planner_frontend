f2f8206e2884fb7c74086d40b23d6a8abbd56f57
dborchuk <frunk.lern@gmail.com>
2020-04-26 14:04:44 +0300

    Added message for user without username

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { withRouter } from 'react-router-dom'
 import qs from 'query-string'
 import { withApollo } from 'react-apollo'
+import { Alert } from 'antd'
 import * as R from 'ramda'
 import { SIGN_IN_MUTATION } from 'api'
 import { decode } from '../../utils/jwt'
 import { UserContext } from '../../context/userContext'
+import { Box } from 'noui/Position'
 
 const Login = ({ location, history, client }) => {
   const { setUser } = useContext(UserContext)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     (async () => {
@@ -32,24 +35,39 @@ const Login = ({ location, history, client }) => {
         authDate: auth_date,
       }
 
-      let { data: { signIn: token } } = await client.mutate({
-        mutation: SIGN_IN_MUTATION,
-        variables: userData,
-      })
+      try {
+        let {
+          data: { signIn: token },
+        } = await client.mutate({
+          mutation: SIGN_IN_MUTATION,
+          variables: userData,
+        })
 
-      localStorage.setItem('AUTH_DATA', token)
-      setUser(decode(token))
+        localStorage.setItem('AUTH_DATA', token)
+        setUser(decode(token))
 
-      history.replace('/')
+        history.replace('/')
+      } catch (err) {
+        setError(err)
+      }
     })()
   }, [])
 
-  return (
-    <div>Login</div>
-  )
+  if (error) {
+    return (
+      <Box pt="10px">
+        <Alert
+          message={error.graphQLErrors.map(err => err.message).join(', ')}
+          type="error"
+        />
+      </Box>
+    )
+  }
+
+  return <div>Please wait...</div>
 }
 
 export default R.compose(
   withRouter,
-  withApollo
+  withApollo,
 )(Login)
