03698c967fd2b817ff25dee3d99ff4e52242aea3
Alex <ldevlnull@users.noreply.github.com>
2020-06-04 19:12:32 +0300

    filter null in string for user f- and sname (#66)

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -2,7 +2,7 @@ import { Avatar, Tooltip } from 'antd'
 import React from 'react'
 
 const UserInfo = ({ firstName, lastName, username, avatar, position = 'top' }) =>
-  <Tooltip placement={position} title={`${firstName} ${lastName}`}>
+  <Tooltip placement={position} title={`${firstName || ''} ${lastName || ''}`}>
     <a
       href={`https://web.telegram.org/#/im?p=@${username}`}
       target="_blank"
