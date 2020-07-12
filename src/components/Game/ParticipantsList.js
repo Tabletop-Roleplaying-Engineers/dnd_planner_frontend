18d4ee225bad7c3cad46be83b5b0d4cbea32bb8d
Dmytro Borchuk <frunk.lern@gmail.com>
2020-07-12 17:45:36 +0300

    Removed redundant request (#74)
    Co-authored-by: Dmytro Borchuk <frunk.lern@gmail.com>
    Co-authored-by: rosty <blackswordgc@gmail.com>
    Co-authored-by: Alex <ldevlnull@users.noreply.github.com>

--- src/components/Game/ParticipantsList.js
+++ src/components/Game/ParticipantsList.js
@@ -3,14 +3,15 @@ import { Row, Col, Card } from 'antd'
 import Character from 'components/Character'
 
 export const ParticipantsList = ({ characters }) => {
-  return characters.map(character => (
-    <Row key={character.id}>
-      <Col md={12}>
-        <Card size="small" bordered={false}>
-          <Character {...character} />
-        </Card>
-      </Col>
+  return (
+    <Row>
+      {characters.map(character => (
+        <Col key={character.id} md={12}>
+          <Card size="small" bordered={false}>
+            <Character {...character} />
+          </Card>
+        </Col>
+      ))}
     </Row>
-  ))
+  )
 }
-
