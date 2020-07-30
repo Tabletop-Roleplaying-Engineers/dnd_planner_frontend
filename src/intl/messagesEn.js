98d46db0839ff585a722f2ed65cfac83f9a51362
DBorchuk <frunk.lern@gmail.com>
2020-07-31 00:27:08 +0300

    feat: intl participation list

--- src/intl/messagesEn.js
+++ src/intl/messagesEn.js
@@ -1,4 +1,8 @@
 export const messages = {
   availableSlots: 'Available slots: {count} / {max}',
   availableLevels: 'Available levels',
+  'character.removeFromGameBtn': 'Remove from game',
+  'character.removeFromGameDialogHeader': 'Remove character from game',
+  'character.removeFromGameDialogContent':
+    'Are you sure that you want to remove {name} from this game?',
 }
--- src/intl/messagesUa.js
+++ src/intl/messagesUa.js
@@ -1,4 +1,8 @@
 export const messages = {
   availableSlots: 'Доступні слоти: {count} / {max}',
   availableLevels: 'Доступні рівні',
+  'character.removeFromGameBtn': 'Видалити з гри',
+  'character.removeFromGameDialogHeader': 'Видалити персонажа з гри',
+  'character.removeFromGameDialogContent':
+    'Ви впевнені, що хочете видалити {name} з цієї гри?',
 }
