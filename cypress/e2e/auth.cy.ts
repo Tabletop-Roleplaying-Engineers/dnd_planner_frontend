e46ba8322164f38f0953b36cb10d2207fd294c7c
dborchuk <frunk.lern@gmail.com>
2022-12-27 21:46:52 +0200

    Removed useless tests

--- cypress/e2e/auth.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('Authentication', function () {
-  it('user should be able to login', function () {
-    cy.visit('/')
-    cy.get('[data-testid=login-btn]').click()
-    cy.get('[data-testid=profile-drop-menu]').should('be.visible')
-  })
-})
